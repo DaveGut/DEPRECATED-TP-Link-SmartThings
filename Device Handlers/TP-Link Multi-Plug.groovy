d9519adf0865a97272cf2da8c16393207c86e8c3
David Gutheinz <djgutheinz@outlook.com>
2019-03-09 08:43:14 -0600

    Add files via upload

--- Device Handlers/TP-Link Multi-Plug.groovy
+++ Device Handlers/TP-Link Multi-Plug.groovy
@@ -20,39 +20,79 @@ All  development is based upon open-source data on the TP-Link devices; primaril
 ======== DO NOT EDIT LINES BELOW ===========================*/
 	def devVer()	{ return "4.0.01" }
 metadata {
-	definition (name: "TP-Link Smart Multi-Plug", 
-    			namespace: "davegut", 
-                author: "Dave Gutheinz, Anthony Ramirez", 
-                ocfDeviceType: "oic.d.smartplug", 
-                mnmn: "SmartThings", 
-                vid: "generic-switch") {
+	definition (name: "TP-Link Smart Energy Monitor Plug",
+				namespace: "davegut",
+				author: "Dave Gutheinz and Anthony Ramirez",
+				ocfDeviceType: "oic.d.smartplug",
+				mnmn: "SmartThings",
+				vid: "generic-switch-power-energy") {
 		capability "Switch"
 		capability "refresh"
 		capability "Health Check"
+		capability "Power Meter"
+		command "getPower"
+		capability "Energy Meter"
+		command "getEnergyStats"
+		attribute "monthTotalE", "string"
+		attribute "monthAvgE", "string"
+		attribute "weekTotalE", "string"
+		attribute "weekAvgE", "string"
 	}
 	tiles(scale: 2) {
-		multiAttributeTile(name: "switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
 			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-				attributeState "on", label:'${name}', action: "switch.off", icon: "st.Appliances.appliances17", backgroundColor: "#00a0dc",
-				nextState: "waiting"
-				attributeState "off", label:'${name}', action: "switch.on", icon: "st.Appliances.appliances17", backgroundColor: "#ffffff",
-				nextState: "waiting"
-				attributeState "waiting", label:'${name}', action: "switch.on", icon: "st.Appliances.appliances17", backgroundColor: "#15EE10",
-				nextState: "waiting"
-				attributeState "Unavailable", label:'Unavailable', action: "switch.on", icon: "st.Appliances.appliances17", backgroundColor: "#e86d13",
-				nextState: "waiting"
+				attributeState "on", label:'${name}', action:"switch.off", icon:"st.Appliances.appliances17", backgroundColor:"#00a0dc",
+				nextState:"waiting"
+				attributeState "off", label:'${name}', action:"switch.on", icon:"st.Appliances.appliances17", backgroundColor:"#ffffff",
+				nextState:"waiting"
+				attributeState "waiting", label:'${name}', action:"switch.on", icon:"st.Appliances.appliances17f", backgroundColor:"#15EE10",
+				nextState:"waiting"
+				attributeState "commsError", label:'Comms Error', action:"switch.on", icon:"st.Appliances.appliances17", backgroundColor:"#e86d13",
+				nextState:"waiting"
 			}
-			tileAttribute ("deviceError", key: "SECONDARY_CONTROL") {
+ 			tileAttribute ("deviceError", key: "SECONDARY_CONTROL") {
 				attributeState "deviceError", label: '${currentValue}'
 			}
 		}
+		
 		standardTile("refresh", "capability.refresh", width: 2, height: 1, decoration: "flat") {
-			state "default", label: "Refresh", action: "refresh.refresh"
+			state "default", label:"Refresh", action:"refresh.refresh"
 		}
+		
+		valueTile("currentPower", "device.power", decoration: "flat", height: 1, width: 2) {
+			state "power", label: 'Current Power \n\r ${currentValue} W'
+		}
+
+		valueTile("energyToday", "device.energy", decoration: "flat", height: 1, width: 2) {
+			state "energy", label: 'Usage Today\n\r${currentValue} WattHr'
+		}
+
+		valueTile("monthTotal", "device.monthTotalE", decoration: "flat", height: 1, width: 2) {
+			state "monthTotalE", label: '30 Day Total\n\r ${currentValue} KWH'
+		}
+
+		valueTile("monthAverage", "device.monthAvgE", decoration: "flat", height: 1, width: 2) {
+			state "monthAvgE", label: '30 Day Avg\n\r ${currentValue} KWH'
+		}
+ 
+		valueTile("weekTotal", "device.weekTotalE", decoration: "flat", height: 1, width: 2) {
+			state "weekTotalE", label: '7 Day Total\n\r ${currentValue} KWH'
+		}
+
+		valueTile("weekAverage", "device.weekAvgE", decoration: "flat", height: 1, width: 2) {
+			state "weekAvgE", label: '7 Day Avg\n\r ${currentValue} KWH'
+		}
+
+		valueTile("4x1Blank", "default", decoration: "flat", height: 1, width: 4) {
+			state "default", label: ''
+		}
+
 		main("switch")
-		details("switch", "refresh")
+		details("switch", "refresh" ,"4x1Blank",
+				"currentPower", "weekTotal", "monthTotal",
+				"energyToday", "weekAverage", "monthAverage")
 	}
-    
+
     def refreshRate = [:]
     refreshRate << ["1" : "Refresh every minute"]
     refreshRate << ["5" : "Refresh every 5 minutes"]
@@ -63,8 +103,7 @@ metadata {
 	preferences {
 		input ("refresh_Rate", "enum", title: "Device Refresh Rate", options: refreshRate)
 		input ("device_IP", "text", title: "Device IP (Hub Only, NNN.NNN.N.NNN)")
-		input ("gateway_IP", "text", title: "Hub IP (Hub Only, NNN.NNN.N.NNN)")
-		input ("plug_No", "text", title: "Plug No (Hub Only, Number of the plug (00, 01, 02, etc)")
+		input ("gateway_IP", "text", title: "Gateway IP (Hub Only, NNN.NNN.N.NNN)")
 	}
 }
 
@@ -94,55 +133,244 @@ def updated() {
 
     if (device_IP) { updateDataValue("deviceIP", device_IP) }
     if (gateway_IP) { updateDataValue("gatewayIP", gateway_IP) }
+	sendEvent(name: "DeviceWatch-Enroll", value: groovy.json.JsonOutput.toJson(["protocol":"cloud", "scheme":"untracked"]), displayed: false)
     if (getDataValue("installType") == "Manual") { updateDataValue("deviceDriverVersion", devVer())  }
-    if (!getDataValue("plugId")) {
-	    if (getDataValue("installType")== "Manual" && getDataValue("deviceIP") && getDataValue("deviceIP") && plug_No) {
-			sendCmdtoServer('{"system" :{"get_sysinfo" :{}}}', "deviceCommand", "parsePlugId")
-        }
-    } else { 
-    	runIn(2, refresh)
-		sendEvent(name: "DeviceWatch-Enroll", value: groovy.json.JsonOutput.toJson(["protocol":"cloud", "scheme":"untracked"]), displayed: false)
-	}
-}
-
-def parsePlugId(cmdResponse) {
-	def deviceData = cmdResponse.system.get_sysinfo
-	def plugId = "${deviceData.deviceId}${plug_No}"
-	updateDataValue("plugId", plugId)
-	log.info "${device.name} ${device.label}: Plug ID set to ${plugId}"
+    
+	schedule("0 05 0 * * ?", setCurrentDate)
+	schedule("0 10 0 * * ?", getEnergyStats)
+	setCurrentDate()
+	runIn(2, refresh)
+	runIn(7, getEnergyStats)
 }
 
 //	===== Basic Plug Control/Status =====
 def on() {
-	def plugId = getDataValue("plugId")
-	sendCmdtoServer("""{"context":{"child_ids":["${plugId}"]},"system":{"set_relay_state":{"state": 1}}}""",
-					"deviceCommand", "commandResponse")
+	sendCmdtoServer('{"system":{"set_relay_state":{"state": 1}}}', "deviceCommand", "commandResponse")
+	runIn(2, refresh)
 }
 
 def off() {
-	def plugId = getDataValue("plugId")
-	sendCmdtoServer("""{"context":{"child_ids":["${plugId}"]},"system":{"set_relay_state":{"state": 0}}}""",
-					"deviceCommand", "commandResponse")
+	sendCmdtoServer('{"system":{"set_relay_state":{"state": 0}}}', "deviceCommand", "commandResponse")
+	runIn(2, refresh)
+}
+
+def getSystemInfo() {
+	sendCmdtoServer('{"system":{"get_sysinfo":{}}}', "deviceCommand", "refreshResponse")
+    runIn(2, getPower)
 }
 
 def refresh(){
-	sendCmdtoServer('{"system" :{"get_sysinfo" :{}}}', "deviceCommand", "refreshResponse")
+	sendCmdtoServer('{"system":{"get_sysinfo":{}}}', "deviceCommand", "refreshResponse")
+	runIn(2, getPower)
+    runIn(7, getConsumption)
 }
 
 def refreshResponse(cmdResponse){
-	def children = cmdResponse.system.get_sysinfo.children
-	def plugId = getDataValue("plugId")
-	children.each {
-		if (it.id == plugId) {
-			if (it.state == 1) {
-				sendEvent(name: "switch", value: "on")
-				log.info "${device.label}: Power: on"
-			} else {
-				sendEvent(name: "switch", value: "off")
-				log.info "${device.label}: Power: off"
+	def status = cmdResponse.system.get_sysinfo.relay_state
+	if (status == 1) {
+		status = "on"
+		log.info "${device.label}: Power: on"
+		sendEvent(name: "switch", value: "on")
+	} else {
+		status = "off"
+		log.info "${device.label}: Power: off"
+		sendEvent(name: "switch", value: "off")
+	}
+}
+
+//	===== Get Current Energy Data =====
+def getPower(){
+	sendCmdtoServer("""{"${state.emeterText}":{"get_realtime":{}}}""", "deviceCommand", "energyMeterResponse")
+}
+
+def energyMeterResponse(cmdResponse) {
+	def realtime = cmdResponse["emeter"]["get_realtime"]
+	if (realtime.power == null) {
+		state.powerScale = "power_mw"
+		state.energyScale = "energy_wh"
+	} else {
+		state.powerScale = "power"
+		state.energyScale = "energy"
+	}
+	def powerConsumption = realtime."${state.powerScale}"
+		if (state.powerScale == "power_mw") {
+			powerConsumption = Math.round(powerConsumption/10) / 100
+		} else {
+		powerConsumption = Math.round(100*powerConsumption) / 100
+		}
+	sendEvent(name: "power", value: powerConsumption)
+	log.info "${device.label}: Updated CurrentPower to $powerConsumption"
+}
+
+//	===== Get Today's Consumption =====
+def getConsumption(){
+	sendCmdtoServer("""{"${state.emeterText}":{"get_daystat":{"month": ${state.monthToday}, "year": ${state.yearToday}}}}""", "emeterCmd", "useTodayResponse")
+}
+
+def useTodayResponse(cmdResponse) {
+	def wattHrToday
+	def wattHrData
+	def dayList = cmdResponse["emeter"]["get_daystat"].day_list
+	for (int i = 0; i < dayList.size(); i++) {
+		wattHrData = dayList[i]
+		if(wattHrData.day == state.dayToday) {
+			wattHrToday = wattHrData."${state.energyScale}"
+ 		}
+	}
+	if (state.powerScale == "power") {
+		wattHrToday = Math.round(1000*wattHrToday)
+	}
+	sendEvent(name: "energy", value: wattHrToday)
+	log.info "${device.label}: Updated Usage Today to ${wattHrToday}"
+}
+
+//	===== Get Weekly and Monthly Stats =====
+def getEnergyStats() {
+	state.monTotEnergy = 0
+	state.monTotDays = 0
+	state.wkTotEnergy = 0
+	state.wkTotDays = 0
+	sendCmdtoServer("""{"${state.emeterText}":{"get_daystat":{"month": ${state.monthToday}, "year": ${state.yearToday}}}}""", "emeterCmd", "engrStatsResponse")
+	runIn(4, getPrevMonth)
+}
+
+def getPrevMonth() {
+	def prevMonth = state.monthStart
+	if (state.monthToday == state.monthStart) {
+		//	If all of the data is in this month, do not request previous month.
+		//	This will occur when the current month is 31 days.
+		return
+	} else if (state.monthToday - 2 == state.monthStart) {
+		//	If the start month is 2 less than current, we must handle
+		//	the data to get a third month - January.
+		state.handleFeb = "yes"
+		prevMonth = prevMonth + 1
+		runIn(4, getJan)
+	}
+	sendCmdtoServer("""{"${state.emeterText}":{"get_daystat":{"month": ${prevMonth}, "year": ${state.yearStart}}}}""", "emeterCmd", "engrStatsResponse")
+}
+
+def getJan() {
+//	Gets January data on March 1 and 2.  Only access if current month = 3
+//	and start month = 1
+	sendCmdtoServer("""{"${state.emeterText}":{"get_daystat":{"month": ${state.monthStart}, "year": ${state.yearStart}}}}""", "emeterCmd", "engrStatsResponse")
+}
+
+def engrStatsResponse(cmdResponse) {
+/*	
+	This method parses up to two energy status messages from the device,
+	adding the energy for the previous 30 days and week, ignoring the
+	current day.  It then calculates the 30 and 7 day average formatted
+	in kiloWattHours to two decimal places.
+*/
+	def dayList = cmdResponse[state.emeterText]["get_daystat"].day_list
+	if (!dayList[0]) {
+		log.info "${device.label}: Month has no energy data."
+		return
+	}
+	def monTotEnergy = state.monTotEnergy
+	def wkTotEnergy = state.wkTotEnergy
+	def monTotDays = state.monTotDays
+	def wkTotDays = state.wkTotDays
+    def startDay = state.dayStart
+	def dataMonth = dayList[0].month
+	if (dataMonth == state.monthToday) {
+		for (int i = 0; i < dayList.size(); i++) {
+			def energyData = dayList[i]
+			monTotEnergy += energyData."${state.energyScale}"
+			monTotDays += 1
+			if (state.dayToday < 8 || energyData.day >= state.weekStart) {
+				wkTotEnergy += energyData."${state.energyScale}"
+				wkTotDays += 1
+			}
+			if(energyData.day == state.dayToday) {
+				monTotEnergy -= energyData."${state.energyScale}"
+				wkTotEnergy -= energyData."${state.energyScale}"
+				monTotDays -= 1
+				wkTotDays -= 1
+			}
+		}
+	} else if (state.handleFeb == "yes" && dataMonth == 2) {
+    	startDay = 1
+		for (int i = 0; i < dayList.size(); i++) {
+			def energyData = dayList[i]
+			if (energyData.day >= startDay) {
+				monTotEnergy += energyData."${state.energyScale}"
+				monTotDays += 1
+			}
+			if (energyData.day >= state.weekStart && state.dayToday < 8) {
+				wkTotEnergy += energyData."${state.energyScale}"
+				wkTotDays += 1
+			}
+		}
+	} else if (state.handleFeb == "yes" && dataMonth == 1) {
+		for (int i = 0; i < dayList.size(); i++) {
+			def energyData = dayList[i]
+			if (energyData.day >= startDay) {
+				monTotEnergy += energyData."${state.energyScale}"
+				monTotDays += 1
+			}
+			state.handleFeb = ""
+		}
+	} else {
+		for (int i = 0; i < dayList.size(); i++) {
+			def energyData = dayList[i]
+			if (energyData.day >= startDay) {
+				monTotEnergy += energyData."${state.energyScale}"
+				monTotDays += 1
+			}
+			if (energyData.day >= state.weekStart && state.dayToday < 8) {
+				wkTotEnergy += energyData."${state.energyScale}"
+				wkTotDays += 1
 			}
 		}
 	}
+	state.monTotDays = monTotDays
+	state.monTotEnergy = monTotEnergy
+	state.wkTotEnergy = wkTotEnergy
+	state.wkTotDays = wkTotDays
+	log.info "$device.name ${device.label}: Update 7 and 30 day energy consumption statistics"
+    if (monTotDays == 0) {
+    	//	Aviod divide by zero on 1st of month
+    	monTotDays = 1
+        wkTotDays = 1 
+	}
+	def monAvgEnergy =monTotEnergy/monTotDays
+	def wkAvgEnergy = wkTotEnergy/wkTotDays
+	if (state.powerScale == "power_mw") {
+		monAvgEnergy = Math.round(monAvgEnergy/10)/100
+		wkAvgEnergy = Math.round(wkAvgEnergy/10)/100
+		monTotEnergy = Math.round(monTotEnergy/10)/100
+		wkTotEnergy = Math.round(wkTotEnergy/10)/100
+	} else {
+		monAvgEnergy = Math.round(100*monAvgEnergy)/100
+		wkAvgEnergy = Math.round(100*wkAvgEnergy)/100
+		monTotEnergy = Math.round(100*monTotEnergy)/100
+		wkTotEnergy = Math.round(100*wkTotEnergy)/100
+	}
+	sendEvent(name: "monthTotalE", value: monTotEnergy)
+	sendEvent(name: "monthAvgE", value: monAvgEnergy)
+	sendEvent(name: "weekTotalE", value: wkTotEnergy)
+	sendEvent(name: "weekAvgE", value: wkAvgEnergy)
+}
+
+//	===== Obtain Week and Month Data =====
+def setCurrentDate() {
+	sendCmdtoServer('{"time":{"get_time":null}}', "deviceCommand", "currentDateResponse")
+}
+
+def currentDateResponse(cmdResponse) {
+	def currDate =  cmdResponse["time"]["get_time"]
+	state.dayToday = currDate.mday.toInteger()
+	state.monthToday = currDate.month.toInteger()
+	state.yearToday = currDate.year.toInteger()
+	def dateToday = Date.parse("yyyy-MM-dd", "${currDate.year}-${currDate.month}-${currDate.mday}")
+	def monStartDate = dateToday - 30
+	def wkStartDate = dateToday - 7
+	state.dayStart = monStartDate[Calendar.DAY_OF_MONTH].toInteger()
+	state.monthStart = monStartDate[Calendar.MONTH].toInteger() + 1
+	state.yearStart = monStartDate[Calendar.YEAR].toInteger()
+	state.weekStart = wkStartDate[Calendar.DAY_OF_MONTH].toInteger()
 }
 
 //	===== Send the Command =====
@@ -218,8 +446,17 @@ def actionDirector(action, cmdResponse) {
 		case "refreshResponse":
 			refreshResponse(cmdResponse)
 			break
-		case "parsePlugId" :
-			parsePlugId(cmdResponse)
+		case "energyMeterResponse":
+			energyMeterResponse(cmdResponse)
+			break
+		case "useTodayResponse":
+			useTodayResponse(cmdResponse)
+			break
+		case "currentDateResponse":
+			currentDateResponse(cmdResponse)
+			break
+		case "engrStatsResponse":
+			engrStatsResponse(cmdResponse)
 			break
 		default:
 			log.info "${device.label}: Interface Error.	See SmartApp and Device error message."
@@ -243,7 +480,7 @@ def setRefreshRate(refreshRate) {
 			log.info "${device.label}: Refresh Scheduled for every 10 minutes."
 			break
 		case "15" :
-			runEvery15Minutes(refresh)
+			runEvery10Minutes(refresh)
 			log.info "${device.label}: Refresh Scheduled for every 15 minutes."
 			break
 		default:
